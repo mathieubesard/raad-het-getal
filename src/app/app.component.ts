07bb46a23e090220104ec107e9e65e3a965de763
Mathieu Besard <mathieubesard@gmail.com>
2020-10-18 20:52:12 +0200

    Working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'raad-het-getal';
+  randomNumber: number = Math.floor(Math.random()*100);
+  mededeling: string = '';
+  beurtenResterend: number = 10;
+  gok(jenNieuweGok: string) {
+    var jenNieuweGokNummer: Number = Number(jenNieuweGok);
+    if (this.randomNumber == jenNieuweGokNummer) {
+      this.mededeling = 'Je hebt het getal geraden';
+    }
+    else if (this.randomNumber < jenNieuweGokNummer) {
+      this.mededeling = 'Kies een lager nummer';
+    }
+    else {
+      this.mededeling = 'Kies een hoger nummer';
+    }
+    this.beurtenResterend -= 1;
+  }
 }
